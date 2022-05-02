e1c95dc7dd05f553c1991daab08d46ad7c5f8e83
DexxterGWM <99261879+DexxterGWM@users.noreply.github.com>
2022-05-02 16:20:28 -0300

    File

---
+++ Script.py
@@ -0,0 +1,9 @@
+MyName = 'Dexxter'
+MyInsta = '@dexxter.gwm'
+ILove = 'games'
+
+ # With format
+print('\n My name is {}, my instagram is {} and I love {}.'.format(MyName, MyInsta, ILove))
+
+ # With fstring
+print(f'\n My name is {MyName}, my instagram is {MyInsta} and I love {ILove}.')
